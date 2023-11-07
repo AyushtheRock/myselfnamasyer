0863284d318a2a9183a347e4780b7f660cd0174a
Ayush Chaurasia <ayushthegreat220@gmail.com>
2023-11-07 18:00:46 +0530

    my

---
+++ App.js
@@ -0,0 +1,14 @@
+
+
+const parent =React.createElement("div", {id:"parent"} , [
+    React.createElement("div", {id:"child"} ,
+[React.createElement("h1",  {} , "hellow new"),React.createElement("h2",  {} , "hellow new")]),React.createElement("div", {id:"child"} ,
+[React.createElement("h1",  {} , "hellow new"),React.createElement("h2",  {} , "hellow new")])
+]
+);
+
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
